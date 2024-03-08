23083487d00ad8452a68475c37aa03f393bb7bb0
sahil-raj <sahilrajaaru@gmail.com>
2024-03-09 00:52:36 +0530

    contract

--- contracts/Domb.sol
+++ contracts/Domb.sol
@@ -2,11 +2,16 @@
 //contracts/Domb.sol
 pragma solidity ^0.8.0;
 
+//imports of ERC20 and ERC20Capped from openzeppelin
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract Domb is ERC20 {
+//ERC20Capped also inherits ERC20
+contract Domb is ERC20Capped {
     address payable public owner;
-    constructor() {
-        
+    constructor() ERC20("Domb", "DMB") {
+        //msg.sender is not payable by default
+        owner = payable(msg.sender);
+        _mint();
     }
 }
