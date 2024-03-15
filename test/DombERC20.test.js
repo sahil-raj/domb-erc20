42ea9432ad80d852c56e1632b69b7deb15033107
sahil-raj <sahilrajaaru@gmail.com>
2024-03-15 23:57:08 +0530

    test: deployment/1

--- test/DombERC20.test.js
+++ test/DombERC20.test.js
@@ -7,4 +7,10 @@ describe("DombERC20", () => {
         [owner, add1, add2] = await ethers.getSigners();
         refContract = await domb.deploy();
     });
+
+    describe("On deployment", () => {
+        it("Should mint 1000 * (10**decimals())", async () => {
+            expect(await refContract.totalSupply()).to.equal(BigInt(1000000000000000000000));
+        });
+    });
 });
